b0ac2e8e0db216cafbd449c9d12a8e1717cbae9b
wandeltn <niklas.wandelt@gmail.com>
2026-01-10 17:16:57 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @file This is the entrypoint for your project.
+ * If used as a node module, when someone runs
+ * `import stuff from 'your-module'` (typescript)
+ * or `const stuff = require('your-module')` (javascript)
+ * whatever is exported here is what they'll get.
+ * For small projects you could put all your code right in this file.
+ */
+import getPlayerDetails from '@/api.js';
+
+export * from './lib/sample-module.js';
+
+export default getPlayerDetails;
---
+++ src/lib/api.ts
@@ -0,0 +1,68 @@
+import { PlayerDetails } from '$/types/index.js';
+import axios, { AxiosResponse, AxiosRequestConfig, RawAxiosRequestHeaders } from 'axios';
+import * as cheerio from 'cheerio';
+
+function getSearchResults(query: string): Promise<string | never[]> {
+    const client = axios.create({
+        baseURL: 'https://tournamentsoftware.com/find/player',
+        timeout: 5000,
+    });
+
+    return client.get(`/DoSearch?Page=1&SportID=2&Query=${encodeURIComponent(query)}&X-Requested-With=XMLHttpRequest&_=0`)
+        .then((response: AxiosResponse) => {
+            const data: string = response.data;
+            return data;
+        })
+        .catch((error) => {
+            console.error('Error fetching search results:', error);
+            return [];
+        });
+}
+
+
+async function getPlayerDetails(name: string): Promise<PlayerDetails> {
+    const client = axios.create({
+        baseURL: 'https://tournamentsoftware.com/find/player',
+        timeout: 5000,
+    });
+    
+    let result: PlayerDetails;
+    
+    try {
+        const $ = cheerio.load(await getSearchResults(name));
+        const club = $("small.media__subheading span.nav-link__value")
+            .first()
+            .text()
+            .trim();
+        console.log('Extracted Club:', club);
+
+        const playerName = $(
+            "h5.media__title a.media__link span.nav-link__value"
+        )
+        .first()
+        .text()
+        .trim();
+
+        console.log("Player Name:", playerName);
+
+
+        const playerId = $("h5.media__title span.media__title-aside")
+            .first()
+            .text()
+            .replace(/[()]/g, "")
+            .trim();
+
+        console.log("Player ID:", playerId);
+
+        result = { name: playerName, club: club, id: playerId };
+        return result;
+
+    } catch (error) {
+        console.error('Error in getByName:', error);
+        result = { name: '', club: '', id: '' };
+    }
+    
+    return result;    
+}
+
+export default getPlayerDetails;
